05387312e3845a8d854e00a8ff27034a7f6c9dbf
braianflorian <braianflorian@hotmail.com>
2017-08-17 22:20:30 -0600

    fix

--- config/routes.js
+++ config/routes.js
@@ -1,10 +1,10 @@
 module.exports.routes = {
   //user
-  'POST /api/data': 'DataController.create',  
-  'GET /api/data': 'DataController.findAll',  
-  'GET /api/data/:direccion_estacion': 'DataController.findStation',  
-  'GET /api/data/:direccion_estacion/:tipo_sensor': 'DataController.findStationType',  
-  'GET /api/data/:direccion_estacion/:tipo_sensor/:limite_definido': 'DataController.findStationTypeLimit',    
+  'POST /app/data': 'DataController.create',  
+  'GET /app/data': 'DataController.findAll',  
+  'GET /app/data/:direccion_estacion': 'DataController.findStation',  
+  'GET /app/data/:direccion_estacion/:tipo_sensor': 'DataController.findStationType',  
+  'GET /app/data/:direccion_estacion/:tipo_sensor/:limite_definido': 'DataController.findStationTypeLimit',    
   '/': {
     view: 'homepage'
   }
