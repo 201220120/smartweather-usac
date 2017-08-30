0bbfffc0878e0d8b3563621400bbf5515a28f2a3
braianflorian <braianflorian@hotmail.com>
2017-08-29 18:47:24 -0600

    tests

--- api/controllers/AnalitycsController.js
+++ api/controllers/AnalitycsController.js
@@ -12,7 +12,7 @@ module.exports = {
   climate: function (req, res) {
     let direccion_estacion = req.params.direccion_estacion;
     if (!direccion_estacion) return res.badRequest({ err: 'El campo estacion es obligatorio' });
-    sails.log('pep');
+    sails.log('pepi');
     var exec = require('child_process').exec;
     var child = exec('java -jar ' + 'analitycs/Predecir-Clima.jar ' + direccion_estacion,
       function (error, stdout, stderr) {
